b5882a49825399c4a332745210886469205e6429
Peter Jang <peter.x.jang@gmail.com>
2017-08-20 11:03:55 -0500

    Rewrite meetings index to use Vue.js instead of Rails

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -1,12 +1,20 @@
-/* global Vue */
+/* global Vue, $ */
 document.addEventListener("DOMContentLoaded", function(event) { 
   var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      meetings: []
     },
     mounted: function() {
-
+      $.ajax({
+        url: "/api/v1/meetings",
+        type: "GET",
+        success: function(response) {
+          console.log(response);
+          this.meetings = response;
+        }.bind(this)
+      });
     },
     methods: {
 
