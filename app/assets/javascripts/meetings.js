a5dd3b851bfd25f315ee512b8243d221d5ae56f7
Peter Jang <peter.x.jang@gmail.com>
2017-08-21 20:43:48 -0500

    Use google geocoder to make pins for actual meetings

--- app/assets/javascripts/meetings.js
+++ app/assets/javascripts/meetings.js
@@ -21,6 +21,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
         success: function(response) {
           console.log(response);
           this.meetings = response;
+
+          var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 1
+          });
+          var geocoder = new google.maps.Geocoder();
+          this.meetings.forEach(function(meeting) {
+            console.log('the address is', meeting.address, meeting);
+            geocodeAddress(geocoder, map, meeting.address);
+          });
         }.bind(this)
       });
     },
@@ -55,3 +65,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   });
 });
+
+function geocodeAddress(geocoder, resultsMap, address) {
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
